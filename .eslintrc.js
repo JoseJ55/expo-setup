7a151f04b3531e8ac6df908987fc3cd4a8d56733
JoseJ55 <josejimenez826@gmail.com>
2025-02-21 11:34:22 -0800

    added eslint adn prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        singleQuote: true,
+        bracketSameLine: true,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
